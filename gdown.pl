70a5da5e7831974c26f419df89952ed772b44bab
Bandit42 <mklock75@gmail.com>
2022-04-28 05:09:15 -0700

    Update gdown.pl

--- gdown.pl
+++ gdown.pl
@@ -24,10 +24,10 @@ die "\n./gdown.pl 'gdrive file url' [desired file name]\n\n" if $URL eq '';
 my $FILENAME=shift;
 $FILENAME='gdown.'.strftime("%Y%m%d%H%M%S", localtime).'.'.substr(rand,2) if $FILENAME eq '';
 
-if ($URL=~m#^https://drive.google.com/file/d/([^/]+)#) {
+if ($URL=~m#^https?://drive.google.com/file/d/([^/]+)#) {
     $URL="https://docs.google.com/uc?id=$1&export=download";
 }
-elsif ($URL=~m#^https://drive.google.com/open\?id=([^/]+)#) {
+elsif ($URL=~m#^https?://drive.google.com/open\?id=([^/]+)#) {
     $URL="https://docs.google.com/uc?id=$1&export=download";
 }
 
